d87aed02ff83c8d04eae3e6cfb6cd56bf5aefd45
Aynur <113580226+aynurdenikayeva@users.noreply.github.com>
2026-03-01 19:58:39 +0400

    Initial project setup - JWT RBAC Secure API

---
+++ src/main/java/com/aynur/payment/PaymentRbacApiApplication.java
@@ -0,0 +1,13 @@
+package com.aynur.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentRbacApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentRbacApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aynur/payment/PaymentRbacApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aynur.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentRbacApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
